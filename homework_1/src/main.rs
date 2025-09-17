0bdb9ba142fc32532d36dee89033c0b67ce62fa1
14Tamayo <60016874+14Tamayo@users.noreply.github.com>
2025-09-17 15:35:32 +0000

    hw1 test

--- homework_1/src/main.rs
+++ homework_1/src/main.rs
@@ -1,3 +1,14 @@
 fn main() {
-    println!("Hello, world!");
+    //println!("Hello, world!");
+
+    let mut fTemp = 50;
+    
+}
+
+fn fahrenheit_to_celsius(f: f64) -> f64:{
+
+}
+
+fn celsius_to_fahrenheit(c: f64) -> f64:{
+    
 }
