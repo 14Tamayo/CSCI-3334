0d3e1b4b897231d73e6c97c3fe50824219e162cc
14Tamayo <60016874+14Tamayo@users.noreply.github.com>
2025-09-25 04:19:28 +0000

    Practice work on borrowing & ownership

--- homework_1/src/main.rs
+++ homework_1/src/main.rs
@@ -1,14 +1,35 @@
+const FAH_FREEZING: f64  = 32.0;
+
+
 fn main() {
-    //println!("Hello, world!");
+    let temperature: f64 = 100.0;
+    let arr = [33, 34, 35, 36, 37];
 
-    let mut fTemp = 50;
+    let celsiusTemp: f64 = fahrenheit_to_celsius(temperature);
+    let FahrenheitTemp: f64 = celsius_to_fahrenheit(celsiusTemp);
+
+    println!("The temperature in celsius is: {celsiusTemp}");
+    println!("The temperature in fahrenheit is: {FahrenheitTemp}");
+
+    
+
     
 }
 
-fn fahrenheit_to_celsius(f: f64) -> f64:{
 
+fn fahrenheit_to_celsius(fTemp: f64) -> f64 {
+    //Operation
+    let cTemp = (fTemp - 32.0) * (5.0/9.0);
+
+    //return value
+    cTemp
 }
 
-fn celsius_to_fahrenheit(c: f64) -> f64:{
-    
+
+fn celsius_to_fahrenheit(cTemp: f64) -> f64 {
+    //Operation
+    let fTemp = (cTemp * (9.0/5.0)) + 32.0;
+
+    //return value
+    fTemp    
 }
---
+++ homework_1/target/debug/homework_1.d
@@ -0,0 +1,1 @@
+/workspaces/CSCI-3334/homework_1/target/debug/homework_1: /workspaces/CSCI-3334/homework_1/src/main.rs
